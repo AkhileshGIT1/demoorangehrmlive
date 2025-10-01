8af76399844bae10017e5abd6c794476e0d5863c
AkhileshGIT1 <akhileshgolupatidar@gmail.com>
2025-10-01 22:04:49 +0530

    update chrome setting incognito

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import util.ConfigReader;
@@ -14,8 +15,19 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup() {
+    	
+    	// 1. Set up ChromeDriver using WebDriverManager for automatic driver management
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        // 2. Create ChromeOptions object
+        ChromeOptions options = new ChromeOptions();
+
+        // 3. Add the incognito argument
+        options.addArguments("--incognito");
+
+        // 4. Initialize the ChromeDriver with the custom options
+        driver = new ChromeDriver(options);
+    	
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get(ConfigReader.get("url"));
